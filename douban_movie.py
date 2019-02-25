c406c292da1c8c584ae55a3c2fdba99fcb1c82b7
陳思宇 <maskraft@gmail.com>
2019-02-25 22:43:02 +0900

    douban-artists-name

--- douban_movie.py
+++ douban_movie.py
@@ -26,55 +26,67 @@ def get_li(doc):
     soup = BeautifulSoup(doc, 'html.parser')
     ol = soup.find('ol', class_='grid_view')
     name = []  # 名字
+    artists = []  # 导演 演员
     star_con = []  # 评价人数
     score = []  # 评分
     info_list = []  # 短评
     for i in ol.find_all('li'):
         detail = i.find('div', attrs={'class': 'hd'})
+
         movie_name = detail.find(
             'span', attrs={'class': 'title'}).get_text()  # 电影名字
+
+        artist = i.find('div', attrs={'class': 'bd'}).find(
+            'p', attrs={'class': ''}).get_text().strip()  # 导演 演员
+
         level_star = i.find(
             'span', attrs={'class': 'rating_num'}).get_text()  # 评分
+
         star = i.find('div', attrs={'class': 'star'})
         star_num = star.find(text=re.compile('评价'))  # 评价
 
         info = i.find('span', attrs={'class': 'inq'})  # 短评
+
         if info:  # 判断是否有短评
             info_list.append(info.get_text())
         else:
             info_list.append('无')
         score.append(level_star)
-
+        artists.append(artist)
         name.append(movie_name)
         star_con.append(star_num)
     page = soup.find('span', attrs={'class': 'next'}).find('a')  # 获取下一页
     if page:
-        return name, star_con, score, info_list, DOWNLOAD_URL + page['href']
-    return name, star_con, score, info_list, None
+        return name, artists, star_con, score, info_list, DOWNLOAD_URL + page['href']
+    return name, artists, star_con, score, info_list, None
 
 
 def main():
     url = DOWNLOAD_URL
     name = []
+    artists = []
     star_con = []
     score = []
     info = []
     while url:
         doc = download_page(url)
-        movie, star, level_num, info_list, url = get_li(doc)
+        movie, people, star, level_num, info_list, url = get_li(doc)
         name = name + movie
+        artists = artists + people
         star_con = star_con + star
         score = score + level_num
         info = info + info_list
-    for (i, m, o, p) in zip(name, star_con, score, info):
+    for (i, m, o, p, q) in zip(name, artists, star_con, score, info):
         col_A = 'A%s' % (name.index(i) + 1)
         col_B = 'B%s' % (name.index(i) + 1)
         col_C = 'C%s' % (name.index(i) + 1)
         col_D = 'D%s' % (name.index(i) + 1)
+        col_E = 'E%s' % (name.index(i) + 1)
         ws1[col_A] = i
         ws1[col_B] = m
         ws1[col_C] = o
         ws1[col_D] = p
+        ws1[col_E] = q
     wb.save(filename=dest_filename)
 
 
