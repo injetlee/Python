a3200cc31920a1b2a0ec7d900277fac9bee980d0
mjy0424 <65580735+mjy0424@users.noreply.github.com>
2020-07-28 12:37:26 +0800

    修改注释

--- 爬虫集合/meizitu.py
+++ 爬虫集合/meizitu.py
@@ -6,14 +6,11 @@ from bs4 import BeautifulSoup
 
 
 def download_page(url):
-    '''
-    用于下载页面
-    '''
+    #用于下载页面
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0"}
     r = requests.get(url, headers=headers)
     r.encoding = 'gb2312'
     return r.text
-
 
 def get_pic_list(html):
     '''
