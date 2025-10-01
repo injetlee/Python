06bebea5aeae19ff71cf3967d4353d7f2a3f558b
gikol876934 <tmdi161202@gmail.com>
2025-10-01 14:43:52 +0200

    Update countPm.py

--- countPm.py
+++ countPm.py
@@ -7,7 +7,69 @@ def count_pm(*args):
     	result.append(generate_iso_code(pm_abs))
     print (result)
     return result
-    	
+    	// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+
+abstract contract Employee
+{
+    uint public idNumber;
+    uint public managerId;
+
+    constructor(uint _idNumber, uint _managerId)
+    {
+        idNumber = _idNumber;
+        managerId = _managerId;
+    }
+
+    function getAnnualCost() public virtual returns (uint);
+}
+
+contract Salaried is Employee
+{
+    uint public annualSalary;
+
+    constructor(uint _idNumber, uint _managerId, uint _annualSalary)
+        Employee(_idNumber, _managerId)
+    {
+        annualSalary = _annualSalary;
+    }
+
+    function getAnnualCost() public override view returns (uint)
+    {
+        return annualSalary;
+    }
+}
+
+contract Hourly is Employee
+{
+    uint public hourlyRate;
+
+    constructor(uint _idNumber, uint _managerId, uint _hourlyRate) Employee(_idNumber, _managerId)
+    {
+        hourlyRate = _hourlyRate;
+    }
+
+    function getAnnualCost() public override view returns (uint)
+    {
+        return hourlyRate * 2080;
+    }
+}
+
+contract Manager
+{
+    uint[] public employeeIds;
+
+    function addReport(uint _reportId) public 
+    {
+        employeeIds.push(_reportId);
+    }
+
+    function resetReports() public 
+    {
+        delete employeeIds;
+
 def generate_iso_code(x):
 	pm_value = [0.01,0.02,0.04,0.08,0.16,0.32,0.64,1.3,2.5,5,10,20,40,80]  #颗粒浓度
 	iso = list(range(1,25))   #iso级别，共24级
